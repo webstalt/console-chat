5c80727d1bab36a23ddbe39f36856f30b5a1b981
bacravaj <126248255+bacravaj@users.noreply.github.com>
2023-03-12 02:47:36 +0300

    Struckture been reworked

--- ConversationBase/Conversation.h
+++ ConversationBase/Conversation.h
@@ -1,5 +1,7 @@
 #pragma once
 
 class Conversation {
-
+public:
+private:
+	std::pair <User, User> 
 };
--- ConversationBase/Message.h
+++ ConversationBase/Message.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <string>
+#include "user\User.h"
+class Message {
+public:
+	Message(const std::string& content,
+			const User& from,
+			const User& to) :
+				_content(content), _from(from), _to(to) {};
+	std::string GetContent()const;
+	std::string GetSender()const;
+	std::string GetRecipient()const;
+	bool IsRead()const;
+	void MarkAsRead();
+	~Message() = default noexcept;
+private:
+	std::string _content;
+	User _from;
+	User _to;
+	bool _is_read = false;
+};
