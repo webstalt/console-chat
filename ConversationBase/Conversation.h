ee21f7d67027c813b6548e80fa128c78e074421b
bacravaj <126248255+bacravaj@users.noreply.github.com>
2023-03-12 03:04:12 +0300

    Basic methods added to "Conversation.h"

--- ConversationBase/Conversation.h
+++ ConversationBase/Conversation.h
@@ -1,7 +1,18 @@
 #pragma once
+#include "Message.h"
+#include <vector>
+#include <map>
 
 class Conversation {
 public:
+	void SendMessage(const Message&);
+	void ShowNew()const;
+	void ShowAll()const;
+
 private:
-	std::pair <User, User> 
+	std::pair <User, User> _conversation_key;
+	std::map <_conversation_key, std::vector <std::string>> _correspondence;
+private:
+	std::pair <User, User> MakeKey(const User&, const User&);
+	Conversation(const Message&);
 };
