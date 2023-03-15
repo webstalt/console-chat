9edc0f3d97421ea94ed1325907eceda60b1af425
bacravaj <126248255+bacravaj@users.noreply.github.com>
2023-03-15 22:17:07 +0300

    testing 3

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ void UBtest() {
     User u("u", "logintolik", "123321");
     ub->AddUser(u);
     User u1("u1", "login1aaaa", "123ads");
-    ub->AddUser(u1);;
+    ub->AddUser(u1);
     ub->AddUser({ "Anton", "bacravaj", "sa11" });
     ub->AddUser({ "Maria", "webstalt", "sa113sa" });
     /*std::cout << userBase.getUsers() << std::endl;
