97bdd8976a0b3b3d03050029fb5d282866ed14fb
bacravaj <126248255+bacravaj@users.noreply.github.com>
2023-03-19 22:18:06 +0300

    set changed to map; required exception handler

--- user/UserBase.cpp
+++ user/UserBase.cpp
@@ -3,7 +3,7 @@
 #include "UserBase.h"
 
 UserBase::UserBase() {
-	std::set<User> _user_base_data;
+	std::map<std::string, User> _user_base_data;
 }
 UserBase* UserBase::_user_base = nullptr;
 UserBase* UserBase::GetUserBase() {
@@ -12,43 +12,20 @@ UserBase* UserBase::GetUserBase() {
 	}
 	return _user_base;
 }
-//struct find_by_name {
-//  find_by_name(const std::string& name): _name(name) 
-//  {};
-//  bool operator()(const User& user) 
-//  {
-//    return user.setName(name);
-//  };
-//private:
-//  std::string _name;
-//};
-//struct find_by_login {
-//  find_by_login(const std::string& login): _login(login) 
-//  {};
-//  bool operator()(const User& user) 
-//  {
-//    return user.setLogin(login);
-//  };
-//private:
-//  std::string _login;
-//};
-std::set<User> UserBase::GetUsers() const
+std::map<std::string, User> UserBase::GetUsers() const
 {
 	return GetUserBase()->_user_base_data;
 };
 void UserBase::AddUser(const User& user)
 {
-	GetUserBase()->_user_base_data.insert(user);
+	GetUserBase()->_user_base_data[user.GetLogin()] = user;
 };
 size_t UserBase::GetUsersAmount() const
 {
 	return GetUsers().size();
 };
-//std::set<User>::iterator UserBase::getUserByName(const std::string& name) const
-//{
-//  return std::find_if(_userBase.begin(), _userBase.end(), find_by_name(name));
-//};
-//std::set<User>::iterator UserBase::getUserByLogin(const std::string& login) const
-//{
-//  return std::find_if(_userBase.begin(), _userBase.end(), find_by_login(login));
-//};
+User UserBase::GetUserByLogin(const std::string& login)
+{
+	//try catch
+	return GetUserBase()->_user_base_data.at(login);
+};
--- user/UserBase.h
+++ user/UserBase.h
@@ -1,20 +1,19 @@
 #pragma once
 
 #include "User.h"
-#include <set>
+#include <map>
 
 class UserBase {
 public:
 	UserBase(UserBase&) = delete;
 	void operator=(const UserBase&) = delete;
 	static UserBase* GetUserBase();
-	void AddUser(const User& user);
-	std::set<User> GetUsers() const;
+	void AddUser(const User&);
+	std::map<std::string, User> GetUsers() const;
 	size_t GetUsersAmount() const;
-	//std::set<User>::iterator getUserByName(const std::string& name) const;
-	//std::set<User>::iterator getUserByLogin(const std::string& login) const;
+	User GetUserByLogin(const std::string&);
 private:
-	std::set<User> _user_base_data;
+	std::map<std::string,User> _user_base_data;
 	static UserBase* _user_base;
 	UserBase();
 };
