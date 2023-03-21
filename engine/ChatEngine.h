3d9d03f1c015e16148ff4abc8dbf8db94ded263d
bacravaj <126248255+bacravaj@users.noreply.github.com>
2023-03-21 20:33:46 +0300

    addeg ChatEngine.cpp; changed ChatEngine.h (not redy)

--- engine/ChatEngine.h
+++ engine/ChatEngine.h
@@ -3,11 +3,13 @@
 #include <iostream>
 #include "../conversation/ConversationBase.h"
 #include "../user/UserBase.h"
+class State;
 class ChatEngine {
 public:
 	ChatEngine(ChatEngine&) = delete;
 	void operator=(const ChatEngine&) = delete;
-	static void SetEngineState(const State* s);
+	static ChatEngine* GetChatEngine();
+	void SetEngineState(State*);
 	void GOTO_StartMenu();
 	void GOTO_RegistrationMenu();
 	void GOTO_MainMenu();
@@ -15,11 +17,13 @@ public:
 	void GOTO_AllConversations();
 	void GOTO_StartChatting();
 private:
-	static class State* curent_state;
-	ChatEngine();//run Unautorised with initializer of ChatEngine
+	class State* _curent_state;
+	static ChatEngine* _chat_engine;
+	ChatEngine();
 };
 
 class State {
+	virtual void Execute();
 	virtual void GOTO_StartMenu(ChatEngine*);
 	virtual void GOTO_RegistrationMenu(ChatEngine*);
 	virtual void GOTO_MainMenu(ChatEngine*);
@@ -33,49 +37,59 @@ class State {
 //automaticly in GOTO_... functions) 
 class Unautorised : public State {
 public:
-	void Execute();
-	void GOTO_RegistrationMenu(ChatEngine*);
-	void GOTO_MainMenu(ChatEngine*);
-private:
 	Unautorised();
+	void Execute();
+private:
+	void GOTO_RegistrationMenu(ChatEngine*, User*);
+	void GOTO_MainMenu(ChatEngine*, User*);
 };
 class Registration : public State {
 public:
-	void Execute();
-	void GOTO_StartMenu(ChatEngine*);
-	void GOTO_MainMenu(ChatEngine*);
-private:
 	Registration();
+	void Execute(User*);
+private:
+	void GOTO_StartMenu(ChatEngine*);
+	void GOTO_MainMenu(ChatEngine*, User*);
+private:
+	User* _curent_user;
 };
 class MainMenu : public State {
 public:
-	void Execute();
-	void GOTO_StartMenu(ChatEngine*);
-	void GOTO_ChangeProfileMenu(ChatEngine*);
-	void GOTO_AllConversations(ChatEngine*);
-private:
 	MainMenu();
+	void Execute(User*);
+private:
+	void GOTO_StartMenu(ChatEngine*);
+	void GOTO_ChangeProfileMenu(ChatEngine*, User*);
+	void GOTO_AllConversations(ChatEngine*, User*);
+private:
+	User* _curent_user;
 };
 class ProfileSettings : public State {
 public:
-	void Execute();
-	void GOTO_MainMenu(ChatEngine*);
-private:
+	void Execute(User*);
 	ProfileSettings();
+private:
+	void GOTO_MainMenu(ChatEngine*, User*);
+private:
+	User* _curent_user;
 };
 class ChatObserver : public State {
 public:
-	void Execute();
-	void GOTO_StartMenu(ChatEngine*);
-	void GOTO_StartChatting(ChatEngine*);
-private:
 	ChatObserver();
+	void Execute(User*);
+private:
+	void GOTO_StartMenu(ChatEngine*, User*);
+	void GOTO_StartChatting(ChatEngine*, User*);
+private:
+	User* _curent_user;
 };
 class Chatting : public State {
 public:
+	Chatting(User*);
 	void Execute();
-	void GOTO_StartMenu(ChatEngine*);
-	void GOTO_AllConversations(ChatEngine*);
 private:
-	Chatting();
+	void GOTO_StartMenu(ChatEngine*, User*);
+	void GOTO_AllConversations(ChatEngine*, User*);
+private:
+	User* _curent_user;
 };
